7240e46069a7dca274260e86137d5e59a618a615
s999796 <92232231+s999796@users.noreply.github.com>
2022-03-19 23:00:51 +0100

    add

---
+++ src/com/company/Main.java
@@ -0,0 +1,25 @@
+package com.company;
+
+public class Main {
+
+    public static void main(String[] args) {
+    whileExample;
+    }
+    public static void forLoopExample(){
+        int i=0;
+        for(; ;){
+        System.out.println("Hello World!");
+        if(i==10){
+            break;
+        }
+        i++;
+        }
+        public static void whileExample(){
+            int i = 0;
+            while (i<10){
+                System.out.println("Hello World!");
+                i++;
+            }
+        }
+    }
+}
